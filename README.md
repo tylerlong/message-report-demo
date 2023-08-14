58449a904de4fe394c0d045c19fb5da265ae7f93
Tyler Long <tyler4long@gmail.com>
2023-08-14 10:32:17 -0700

    Get extension info

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Untitled App
+# Message Report Demo
+
+This is to reproduce the RingCentral Archiver message backup feature.
+
+Just to download and archive all the messages.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello world!');
+import RingCentral from '@rc-ex/core';
+
+const rc = new RingCentral({
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+  server: process.env.RINGCENTRAL_SERVER_URL,
+});
+
+const main = async () => {
+  await rc.authorize({ jwt: process.env.RINGCENTRAL_JWT_TOKEN! });
+  const ext = await rc.restapi().account().extension().get();
+  console.log(JSON.stringify(ext, null, 2));
+};
+
+main();
